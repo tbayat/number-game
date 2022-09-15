ce221c37ddab8ff382cef61a020308cd0d9ce85e
Tina Bayat <tina@Tinas-MacBook-Pro.local>
2022-09-15 15:37:05 +0200

    creat function

--- number-game.py
+++ number-game.py
@@ -1,20 +1,30 @@
 from random import randrange
 
-actual_number = randrange(1, 10)
-won = False
-for round in range(3):
-    guess = int(input("please guess one number: "))
-    if guess > actual_number :
-        print("your guess is more than number")
-    elif guess < actual_number :
-        print( "your guess is less than number")
-    else :
-        won = True
-        break
+def single_player() :
 
-if won :
-    print("you won")
-else :
-    print(f" actual number was {actual_number} and you lost")
+    actual_number = randrange(1, 10)
+    won = False
+    for round in range(3):
+        guess = int(input("please guess one number: "))
+        if guess > actual_number :
+            print("your guess is more than number")
+        elif guess < actual_number :
+            print( "your guess is less than number")
+        else :
+            won = True
+            break
+
+    print (f"actual number was {actual_number}")
+    return won
+
+def main():
+
+    won = single_player()
+    if won :
+        print("you won")
+    else :
+        print("you lost")
+
+main()
 
 
