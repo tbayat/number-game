62ad253511f09cf16f78561d87406145133b66cf
Tina Bayat <tina@Tinas-MacBook-Pro.local>
2022-09-15 09:08:45 +0200

    simple single player game with randome number

--- number-game.py
+++ number-game.py
@@ -1,15 +1,20 @@
 from random import randrange
 
-
 actual_number = randrange(1, 10)
-
+won = False
 for round in range(3):
     guess = int(input("please guess one number: "))
     if guess > actual_number :
         print("your guess is more than number")
     elif guess < actual_number :
         print( "your guess is less than number")
-
     else :
-        print ("you win")
-        
+        won = True
+        break
+
+if won :
+    print("you won")
+else :
+    print(f" actual number was {actual_number} and you lost")
+
+
